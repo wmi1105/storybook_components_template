0232ba559e1057476c9175b58f7b72d942fb836f
wonmi <bwm0622@gmail.com>
2021-12-06 14:32:21 +0900

    init

---
+++ src/page/testPage.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TestPage } from './TestPage';
+
+export default {
+  title: `testPage`,
+  component: TestPage,
+} as ComponentMeta<typeof TestPage>;
+
+const Template: ComponentStory<typeof TestPage> = (args) => <TestPage />
+
+export const Default = Template.bind({})
+Default.args ={}
+
