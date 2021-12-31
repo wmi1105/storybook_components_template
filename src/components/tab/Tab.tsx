8dafca35668d053d1ad04802b58bf1e82fdb3203
wonmi <bwm0622@gmail.com>
2021-12-31 12:14:30 +0900

    [fix]tab 스타일 수정

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -1,23 +1,59 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+
+const options = [
+  {
+    id: "tab1",
+    text: "tab1",
+  },
+  {
+    id: "tab2",
+    text: "tab2",
+  },
+  {
+    id: "tab3",
+    text: "tab3",
+  },
+  {
+    id: "tab4",
+    text: "tab4",
+  },
+];
 
 export function Tab() {
   return (
-    <TabStyled>
-      <div>tab1</div>
-      <div>tab2</div>
-      <div>tab3</div>
+    <TabStyled flex={options.length}>
+      {options.map((obj) => (
+        <TabOption id={obj.id} key={obj.id} active={obj.id === "tab2"}>
+          {obj.text}
+        </TabOption>
+      ))}
     </TabStyled>
   );
 }
 
-const TabStyled = styled.div`
+const TabStyled = styled.div<{ flex: number }>`
   display: flex;
   justify-content: space-around;
   box-sizing: border-box;
   > div {
-    flex: 3;
-    border: 1px solid gray;
+    ${({ flex }) =>
+      css`
+        flex: ${flex};
+      `}
+
     text-align: center;
     padding: 10px 5px;
   }
+`;
+
+const TabOption = styled.div<{ active: boolean }>`
+  ${({ active }) =>
+    active
+      ? css`
+          border-bottom: 3px solid #4ac6ff;
+        `
+      : css`
+          border-bottom: 1px solid #d8d8d8;
+        `}
 `;
