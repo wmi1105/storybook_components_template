18e1deebdc899f4c3019e676174ad8d638b1a6d1
wonmi <bwm0622@gmail.com>
2022-01-13 15:50:37 +0900

    정리

--- src/components/FormItem/FormItem.stories.tsx
+++ src/components/FormItem/FormItem.stories.tsx
@@ -1,27 +1,29 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Input, INPUT_LINE_THEME, INPUT_STYLE_THEME } from "../input";
-import { FormItem } from "./FormItem";
+// import { ComponentStory, ComponentMeta } from "@storybook/react";
+// import { Input, INPUT_LINE_THEME, INPUT_STYLE_THEME } from "../input";
+// import { FormItem } from "./FormItem";
 
-export default {
-  title: `components/FormItem`,
-  component: FormItem,
-} as ComponentMeta<typeof FormItem>;
+// export default {
+//   title: `components/FormItem`,
+//   component: FormItem,
+// } as ComponentMeta<typeof FormItem>;
 
-const Template: ComponentStory<typeof FormItem> = (args) => {
-  return (
-    <FormItem>
-      <Input
-        label="제목"
-        styleTheme={INPUT_STYLE_THEME.BOTTOMLINE}
-        lineTheme={INPUT_LINE_THEME.HORIZONTAL}
-        value={""}
-        onChange={() => console.log("")}
-      />
-    </FormItem>
-  );
-};
+// const Template: ComponentStory<typeof FormItem> = (args) => {
+//   return (
+//     <FormItem>
+//       <Input
+//         label="제목"
+//         styleTheme={INPUT_STYLE_THEME.BOTTOMLINE}
+//         lineTheme={INPUT_LINE_THEME.HORIZONTAL}
+//         value={""}
+//         onChange={() => console.log("")}
+//       />
+//     </FormItem>
+//   );
+// };
 
-export const Default = Template.bind({});
-Default.args = {};
+// export const Default = Template.bind({});
+// Default.args = {};
 
-//form안에 요소가 2개 이상일 떄
+// //form안에 요소가 2개 이상일 떄
+
+export {};
